429ddb24866a66a0d5b02c54111091a87190ce5d
Danylo Korobko <dakorobko@gmail.com>
2024-12-03 14:48:12 +0100

    home work done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => { 
+    const contactList = await readContacts();
+    const newContacts = Array(number).fill(0).map(createFakeContact);
+    const newContactList = [...contactList, ...newContacts];
+    await writeContacts(newContactList);
+};
 
 generateContacts(5);
